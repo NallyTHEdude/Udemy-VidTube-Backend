e2e385aec778dc01998ddcd8ff1199f68d54894e
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-10-23 13:29:51 +0530

    added utils , connected db , setup .env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("entry point is this index.js file");
+import app from './app.js';
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+// configuring dotenv
+dotenv.config({
+    path: "./.env",
+});
+
+// initializing express application
+const PORT = process.env.PORT || 3000;
+console.log(`PORT from env file: ${process.env.PORT}`);
+connectDB()
+    .then(() => {
+        console.log(`Mongodb is perfectly connected`);
+    })
+    .catch((err) => {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1);
+    });
+    
+
+//app listener
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
